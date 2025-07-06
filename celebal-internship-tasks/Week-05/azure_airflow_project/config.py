44bfad7846618fa2ee7ccb1e21bc5a167a32e6cf
AyushmaanSingh21 <72299641+AyushmaanSingh21@users.noreply.github.com>
2025-07-06 11:31:55 +0530

    Delete celebal-internship-tasks/Week-05/azure_airflow_project directory

--- celebal-internship-tasks/Week-05/azure_airflow_project/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-SOURCE_DB_URL = os.getenv("SOURCE_DB_URL")
-AZURE_CONN_STRING = os.getenv("AZURE_CONN_STRING")
-AZURE_CONTAINER_NAME = os.getenv("AZURE_CONTAINER_NAME")
